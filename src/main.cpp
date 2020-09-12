c306fc01d04f597064f5a098bfd1b64e77c2a335
MoAlyousef <may642_2000@hotmail.com>
2020-09-12 19:20:28 +0300

    missing return

--- src/main.cpp
+++ src/main.cpp
@@ -8,5 +8,6 @@ int main(int argc, char **argv) try {
     auto output = cmkr::args::handle_args(argc, argv);
     return fprintf(stdout, "%s\n", output) < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
 } catch (const std::exception &e) {
-    fprintf(stderr, "%s\n", e.what());
+    (void)fprintf(stderr, "%s\n", e.what());
+    return EXIT_FAILURE;
 }
