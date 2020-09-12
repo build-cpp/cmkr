7ec2279a779a0f35f61a29512bd78ff3a687c400
MoAlyousef <may642_2000@hotmail.com>
2020-09-12 16:38:16 +0300

    throw when no cmake.toml is found

--- src/cmake.cpp
+++ src/cmake.cpp
@@ -1,6 +1,7 @@
 #include "cmake.hpp"
 
 #include <filesystem>
+#include <stdexcept>
 #include <toml.hpp>
 
 namespace fs = std::filesystem;
@@ -19,92 +20,92 @@ std::vector<std::string> to_string_vec(
 } // namespace detail
 
 CMake::CMake(const std::string &path, bool build) {
-    if (fs::exists(fs::path(path) / "cmake.toml")) {
-        if (build) {
-            const auto toml = toml::parse("cmake.toml");
-            if (toml.contains("cmake")) {
-                const auto &cmake = toml::find(toml, "cmake");
+    if (!fs::exists(fs::path(path) / "cmake.toml")) {
+        throw std::runtime_error("No cmake.toml was found!");
+    }
+    if (build) {
+        const auto toml = toml::parse("cmake.toml");
+        if (toml.contains("cmake")) {
+            const auto &cmake = toml::find(toml, "cmake");
 
-                if (cmake.contains("bin-dir")) {
-                    bin_dir = toml::find(cmake, "bin-dir").as_string();
-                }
-
-                if (cmake.contains("generator")) {
-                    generator = toml::find(cmake, "generator").as_string();
-                }
-
-                if (cmake.contains("arguments")) {
-                    gen_args = detail::to_string_vec(toml::find(cmake, "arguments").as_array());
-                }
-            }
-        } else {
-            const auto toml = toml::parse((fs::path(path) / "cmake.toml").string());
-            if (toml.contains("cmake")) {
-                const auto &cmake = toml::find(toml, "cmake");
-                cmake_version = toml::find(cmake, "minimum").as_string();
-
-                if (cmake.contains("cpp-flags")) {
-                    cppflags = detail::to_string_vec(toml::find(cmake, "cpp-flags").as_array());
-                }
-
-                if (cmake.contains("c-flags")) {
-                    cflags = detail::to_string_vec(toml::find(cmake, "c-flags").as_array());
-                }
-
-                if (cmake.contains("link-flags")) {
-                    linkflags = detail::to_string_vec(toml::find(cmake, "link-flags").as_array());
-                }
-
-                if (cmake.contains("subdirs")) {
-                    subdirs = detail::to_string_vec(toml::find(cmake, "subdirs").as_array());
-                }
+            if (cmake.contains("bin-dir")) {
+                bin_dir = toml::find(cmake, "bin-dir").as_string();
             }
 
-            if (toml.contains("project")) {
-                const auto &project = toml::find(toml, "project");
-                proj_name = toml::find(project, "name").as_string();
-                proj_version = toml::find(project, "version").as_string();
+            if (cmake.contains("generator")) {
+                generator = toml::find(cmake, "generator").as_string();
             }
 
-            if (toml.contains("find-package")) {
-                using pkg_map = std::map<std::string, std::string>;
-                packages = toml::find<pkg_map>(toml, "find-package");
+            if (cmake.contains("arguments")) {
+                gen_args = detail::to_string_vec(toml::find(cmake, "arguments").as_array());
+            }
+        }
+    } else {
+        const auto toml = toml::parse((fs::path(path) / "cmake.toml").string());
+        if (toml.contains("cmake")) {
+            const auto &cmake = toml::find(toml, "cmake");
+            cmake_version = toml::find(cmake, "minimum").as_string();
+
+            if (cmake.contains("cpp-flags")) {
+                cppflags = detail::to_string_vec(toml::find(cmake, "cpp-flags").as_array());
             }
 
-            if (toml.contains("fetch-content")) {
-                using content_map = std::map<std::string, std::map<std::string, std::string>>;
-                contents = toml::find<content_map>(toml, "fetch-content");
+            if (cmake.contains("c-flags")) {
+                cflags = detail::to_string_vec(toml::find(cmake, "c-flags").as_array());
             }
 
-            if (toml.contains("bin")) {
-                const auto &bins = toml::find(toml, "bin").as_array();
+            if (cmake.contains("link-flags")) {
+                linkflags = detail::to_string_vec(toml::find(cmake, "link-flags").as_array());
+            }
 
-                for (const auto &bin : bins) {
-                    Bin b;
-                    b.name = toml::find(bin, "name").as_string();
-                    b.type = toml::find(bin, "type").as_string();
+            if (cmake.contains("subdirs")) {
+                subdirs = detail::to_string_vec(toml::find(cmake, "subdirs").as_array());
+            }
+        }
 
-                    b.sources = detail::to_string_vec(toml::find(bin, "sources").as_array());
+        if (toml.contains("project")) {
+            const auto &project = toml::find(toml, "project");
+            proj_name = toml::find(project, "name").as_string();
+            proj_version = toml::find(project, "version").as_string();
+        }
 
-                    if (bin.contains("include-dirs")) {
-                        b.include_dirs =
-                            detail::to_string_vec(toml::find(bin, "include-dirs").as_array());
-                    }
+        if (toml.contains("find-package")) {
+            using pkg_map = std::map<std::string, std::string>;
+            packages = toml::find<pkg_map>(toml, "find-package");
+        }
 
-                    if (bin.contains("link-libs")) {
-                        b.link_libs =
-                            detail::to_string_vec(toml::find(bin, "link-libs").as_array());
-                    }
+        if (toml.contains("fetch-content")) {
+            using content_map = std::map<std::string, std::map<std::string, std::string>>;
+            contents = toml::find<content_map>(toml, "fetch-content");
+        }
 
-                    if (bin.contains("features")) {
-                        b.features = detail::to_string_vec(toml::find(bin, "features").as_array());
-                    }
+        if (toml.contains("bin")) {
+            const auto &bins = toml::find(toml, "bin").as_array();
 
-                    if (bin.contains("defines")) {
-                        b.defines = detail::to_string_vec(toml::find(bin, "defines").as_array());
-                    }
-                    binaries.push_back(b);
+            for (const auto &bin : bins) {
+                Bin b;
+                b.name = toml::find(bin, "name").as_string();
+                b.type = toml::find(bin, "type").as_string();
+
+                b.sources = detail::to_string_vec(toml::find(bin, "sources").as_array());
+
+                if (bin.contains("include-dirs")) {
+                    b.include_dirs =
+                        detail::to_string_vec(toml::find(bin, "include-dirs").as_array());
                 }
+
+                if (bin.contains("link-libs")) {
+                    b.link_libs = detail::to_string_vec(toml::find(bin, "link-libs").as_array());
+                }
+
+                if (bin.contains("features")) {
+                    b.features = detail::to_string_vec(toml::find(bin, "features").as_array());
+                }
+
+                if (bin.contains("defines")) {
+                    b.defines = detail::to_string_vec(toml::find(bin, "defines").as_array());
+                }
+                binaries.push_back(b);
             }
         }
     }
